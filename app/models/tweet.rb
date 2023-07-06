5f2d158cb2910f4bf3bc7165ba54a050c2eac909
takanashi11 <132178818+takanashi11@users.noreply.github.com>
2023-07-06 13:30:35 +0900

    エラーメッセージの日本語対応

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,6 +2,6 @@ class Tweet < ApplicationRecord
   validates :text, presence: true
   belongs_to :user
   has_many :comments  # commentsテーブルとのアソシエーション
-
+  validates :text, :image, presence: true
 
 end
